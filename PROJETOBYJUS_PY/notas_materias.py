e4992e2ddc70c2fec808da58e9fd53f2f6a08ebe
juliacapitaneo <110570649+juliacapitaneo@users.noreply.github.com>
2023-08-17 20:37:35 -0300

    Add files via upload

---
+++ PROJETOBYJUS_PY/notas_materias.py
@@ -0,0 +1,12 @@
+#criação da lista
+result = ["Matemática", 90, "Inglês", 80, "História", 75, "Artes",85]
+
+#exibição da lista geral
+print(result)
+
+#utilização da técnica slicing para exibição
+materias = result[::2]
+print(materias)
+
+notas = result[1::2]
+print(notas)
